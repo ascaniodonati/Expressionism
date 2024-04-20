6ee6f091c99a3f84f37a64b98cc48ac5cc2a5c2e
Ascanio Donati <donatiascanio@gmail.com>
2024-04-20 16:53:53 +0200

    Aggiungere i file di progetto.

---
+++ Expressionism/Expressionism.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Expressionism
+{
+    public class Expressionism
+    {
+        public Expressionism()
+        {
+
+        }
+
+        public string ResolveExpression(string expr)
+        {
+            //1. Prima risolviamo le stringhe tra parentesi graffe
+
+        }
+
+        double CalculateOperation(string expr)
+        {
+
+        }
+
+        //23+45=67
+        double CalculateSimpleOperation(string expr)
+        {
+            var result = Regex.Match(expr, Patterns.SIMPLE_OP);
+
+            if (!result.Success)
+                throw new Exception($"L'espressione {expr} ha più di due operatori");
+
+            double op1 = double.Parse(result.Groups[1].Value);
+            string sign = result.Groups[2].Value;
+            double op2 = double.Parse(result.Groups[3].Value);
+
+            switch (sign)
+            {
+                case "+":
+                    return op1 + op2;
+                case "-":
+                    return op1 - op2;
+                case "*":
+                    return op1 * op2;
+                case "/":
+                    return op1 / op2;
+                default:
+                    throw new Exception($"Il segno <{sign}> non è riconosciuto");
+            }
+        }
+    }
+}
---
+++ Expressionism/Patterns.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Expressionism
+{
+    public static class Patterns
+    {
+        public const string MUL_AND_DIV = @"[\*\/]";
+        public const string SUM_AND_SUB = @"[\+\-]";
+
+        //Simple operation (for ex., matchs only 23+45, not 23+45+67)
+        public const string SIMPLE_OP = @"^(\d+)([\+\-\*\/])(\d+)$";
+
+        //23*45*32
+        public const string MORE_OPERATORS = @"\d+(?:{0}\d+)+";
+        public static string MORE_OPERATORS_MD => string.Format(MORE_OPERATORS, MUL_AND_DIV);
+        public static string MORE_OPERATORS_SS => string.Format(MORE_OPERATORS, SUM_AND_SUB);
+
+        //(23+45)
+        public const string INTO = @"\{0}[^\s\{1}]+\{1}";
+        public static string INTO_CURLY_BRACKETS => string.Format(INTO, "{", "}");
+        public static string INTO_SQUARE_BRACKETS => string.Format(INTO, "[", "]");
+        public static string INTO_BRACKETS => string.Format(INTO, "(", ")");
+    }
+}
